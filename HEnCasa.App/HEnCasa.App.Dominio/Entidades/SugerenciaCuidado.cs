e633533f8d29ab80670167fa366f7cfd7b79106f
Andree <baladinom@unal.edu.co>
2021-10-01 19:54:09 -0500

    Revert "Revert "Revert "Revert "Revert "Primer push"""""
    This reverts commit 6a3963d3be9ca1a7ad30823b64ebe5b6dc75fdec.

--- HEnCasa.App/HEnCasa.App.Dominio/Entidades/SugerenciaCuidado.cs
+++
@@ -1,9 +0,0 @@
-namespace HEnCasa.App.Dominio{
-
-    public class SugerenciaCuidado{
-        
-        public int Id {get; set;}
-        public string FechaHora {get; set;}
-        public string Descripcion {get; set;}
-    }
-}
