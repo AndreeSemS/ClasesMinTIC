719beed8fc32426727e6babb4c831486fa14735a
camilinhi <cacostan@unal.edu.co>
2021-10-01 19:39:19 -0500

    Revert "Revert "Primer push""
    This reverts commit 8321acd3d3ee13e57dd0637250e29101197f2203.
    
    Intento fallido

---
+++ HEnCasa.App/HEnCasa.App.Dominio/Entidades/Medico.cs
@@ -0,0 +1,9 @@
+namespace HEnCasa.App.Dominio{
+
+    public class Medico:Persona{
+        
+        public int IdMedico {get; set;} 
+        public string Especialidad {get; set;}
+        public string Codigo {get; set;}
+    }
+}
